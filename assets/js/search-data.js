c64736f1180945615e3504e2d8aa93805502b7d9
lvzeyu <lvzeyu1995@gmail.com>
2025-12-31 06:40:01 +0000

    Deploying to gh-pages from @ lvzeyu/lvzeyu.github.io@2c223e37cd2cd6fa9c594ac38664d67b7f4f4527 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,18 +53,17 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-prospective-students-master-phd",
+          title: 'Prospective Students (Master/PhD)',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+              window.location.href = "/news/prospective_students/";
+            },},{id: "news-研究生の申請について",
+          title: '研究生の申請について',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",handler: () => {
+              window.location.href = "/news/research_student/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
